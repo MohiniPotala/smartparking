f51a4782ecfc3ab0857885a68ec417e8e5648b20
lokeshpotala <potalalokesh7@gmail.com>
2024-03-21 21:04:45 +0530

    first commit

---
+++ app/src/main/java/com/dominicsilveira/parkingsystem/classes/UpiInfo.java
@@ -0,0 +1,14 @@
+package com.dominicsilveira.parkingsystem.classes;
+
+
+public class UpiInfo {
+    public String upiId="6301441994@ybl";
+    public String upiName="Potala Mohini";
+
+    public UpiInfo(){}
+
+    public UpiInfo(String upiId, String upiName){
+        this.upiId=upiId;
+        this.upiName=upiName;
+    }
+}
